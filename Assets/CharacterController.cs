3496f3ec893ff3bb8f7a7da655f2fb858f9ff20a
Jacob Kreiner <kreinerjm@hotmail.com>
2023-01-06 22:11:37 -0500

    Character controller

---
+++ Assets/CharacterController.cs
@@ -0,0 +1,69 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterController : MonoBehaviour
+{
+    public Camera cam;
+    public GameObject reference;
+
+    float accelerationSpeed = 10f;
+    float maxSpeed = 250f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    
+    }
+
+    void FixedUpdate(){
+        LookAtMouse();
+        HandleKeyInput();
+        SyncCameraLocation();
+    }
+
+    void LookAtMouse(){
+        Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+        var rayStepsToGround = ray.origin.y / Mathf.Abs(ray.direction.y);
+        Debug.Log(rayStepsToGround);
+        var intersectionPoint = ray.origin + rayStepsToGround * ray.direction;
+        intersectionPoint.y = transform.position.y;
+
+        Debug.Log(intersectionPoint);
+        reference.transform.position = intersectionPoint;
+
+        transform.LookAt(intersectionPoint);   
+    }
+
+    void HandleKeyInput(){
+        Rigidbody rb = GetComponent<Rigidbody>();
+
+        if(Input.GetKey(KeyCode.W)){
+            rb.AddForce(Vector3.forward * accelerationSpeed);
+        } else if(Input.GetKey(KeyCode.S)){
+            rb.AddForce(Vector3.back * accelerationSpeed);
+        }
+
+        if(Input.GetKey(KeyCode.A)){
+            rb.AddForce(Vector3.left * accelerationSpeed);
+        } else if(Input.GetKey(KeyCode.D)){
+            rb.AddForce(Vector3.right * accelerationSpeed);
+        }
+
+        if(rb.velocity.magnitude > maxSpeed)
+        {
+            rb.velocity = rb.velocity.normalized * maxSpeed;
+        }
+    }
+
+    void SyncCameraLocation(){
+        cam.transform.position = new Vector3(transform.position.x, transform.position.y+10f, transform.position.z-10f);
+    }
+}

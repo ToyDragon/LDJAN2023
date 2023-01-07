e7fc649098f134c1e613e3b3cc50369e3b1409e8
Jacob Kreiner <kreinerjm@hotmail.com>
2023-01-06 23:06:42 -0500

    Character controller

--- Assets/Scripts/CharacterController.cs
+++ Assets/Scripts/CharacterController.cs
@@ -6,8 +6,10 @@ public class CharacterController : MonoBehaviour
 {
     public Camera cam;
 
-    float accelerationSpeed = 10f;
-    float maxSpeed = 250f;
+    public float maxVelocity = 5f;
+    public float acceleration = 0.5f;
+    public float deccelerate = 1f;
+    public float xVel, zVel;
 
     // Start is called before the first frame update
     void Start()
@@ -19,7 +21,6 @@ public class CharacterController : MonoBehaviour
     void Update()
     {
         
-    
     }
 
     void FixedUpdate(){
@@ -44,24 +45,47 @@ public class CharacterController : MonoBehaviour
         Rigidbody rb = GetComponent<Rigidbody>();
 
         if(Input.GetKey(KeyCode.W)){
-            rb.AddForce(Vector3.forward * accelerationSpeed);
+            zVel += acceleration;
+            if(zVel < 0f) zVel += deccelerate;
         } else if(Input.GetKey(KeyCode.S)){
-            rb.AddForce(Vector3.back * accelerationSpeed);
+            zVel -= acceleration;
+            if(zVel > 0f) zVel -= deccelerate;
+        } else if(zVel != 0f){
+            bool negative = zVel < 0;
+            if(negative){
+                zVel = Mathf.Clamp(zVel + deccelerate, zVel, 0f);
+            } else {
+                zVel = Mathf.Clamp(zVel - deccelerate, 0f, zVel);
+            }
         }
 
         if(Input.GetKey(KeyCode.A)){
-            rb.AddForce(Vector3.left * accelerationSpeed);
+            xVel -= acceleration;
+            if(xVel > 0f) xVel -= deccelerate;
         } else if(Input.GetKey(KeyCode.D)){
-            rb.AddForce(Vector3.right * accelerationSpeed);
+            xVel += acceleration;
+            if(xVel < 0f) xVel += deccelerate;
+        } else if(xVel != 0f){
+            bool negative = xVel < 0;
+            if(negative){
+                xVel = Mathf.Clamp(xVel + deccelerate, xVel, 0f);
+            } else {
+                xVel = Mathf.Clamp(xVel - deccelerate, 0f, xVel);
+            }
         }
 
-        if(rb.velocity.magnitude > maxSpeed)
-        {
-            rb.velocity = rb.velocity.normalized * maxSpeed;
+        xVel = Mathf.Clamp(xVel, -maxVelocity, maxVelocity);
+        zVel = Mathf.Clamp(zVel, -maxVelocity, maxVelocity);
+
+        Vector2 velocity = new Vector2(xVel, zVel);
+        if(velocity.magnitude > maxVelocity){
+            velocity = velocity.normalized * maxVelocity;
         }
+
+        transform.Translate(new Vector3(velocity.x * 0.1f, 0f, velocity.y * 0.1f), Space.World);
     }
 
     void SyncCameraLocation(){
-        cam.transform.position = new Vector3(transform.position.x, transform.position.y+10f, transform.position.z-10f);
+        cam.transform.position = new Vector3(transform.position.x, transform.position.y+20f, transform.position.z-20f);
     }
 }

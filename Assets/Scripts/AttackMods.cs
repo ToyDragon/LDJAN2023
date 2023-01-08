e9acd3c84ef00f316f1d7690c081050d07e534ba
Jacob Kreiner <kreinerjm@hotmail.com>
2023-01-08 00:08:51 -0500

    adding mod data

--- Assets/Scripts/AttackMods.cs
+++ Assets/Scripts/AttackMods.cs
@@ -11,15 +11,36 @@ public class AttackMods
     float splashDamageRadius = 5f;
     float splashDamageModifier = 0.25f;
 
-
+    public void ApplyMod(Mod mod){
+        switch(mod.modifies){
+            case "numProj":
+                numberOfProjectiles += mod.intValue;
+                break;
+            case "pierce":
+                piercing = true;
+                break;
+            case "numPierce":
+                numberOfPierces += mod.intValue;
+                break;
+            case "splash":
+                splashDamage = true;
+                break;
+            case "splashRadius":
+                splashDamageRadius += mod.floatValue;
+                break;
+            case "splashMod":
+                splashDamageModifier += mod.floatValue;
+                break;
+        }
+    }
 }
 
 public class Mod{
-    string modifies;
-    float floatValue;
-    int intValue;
+    public string modifies;
+    public float floatValue;
+    public int intValue;
 
     public Mod(string modifies, float fv, int iv){
-        
+
     }
 }
